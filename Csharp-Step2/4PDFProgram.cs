c5904dc39e449becab2071aacc8531a1c6a6b198
vamshi814 <vamshi814.s@gmail.com>
2026-02-23 17:19:24 +0530

    Constructors, Passing array of Class

---
+++ Csharp-Step2/4PDFProgram.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+
+// Array of classes passed through argument
+class PDFFile
+{
+    //string _filename;      fields
+    //string _filelocation;
+    //string _filesize;
+    //string _createddate;
+
+    //properties - controlled access
+    public string FileName { get; set; }
+    public string FileLocation { get; set; }
+    public string FileSize { get; set; }
+    public string CreatedDate { get; set; }
+}
+
+
+class PDFFilesSystem
+{
+    public void ShowPDFFileNamesLength(string[] fileNames)
+    {
+        Console.WriteLine($"The total pdfs are : {fileNames.Length}");
+        //Code here......
+    }
+
+    public void PrintEveryPDFFileName(string[] fileNames)
+    {
+        Console.WriteLine("Printing Every PDF file name");
+        foreach (string fileName in fileNames)
+        {
+            Console.WriteLine($"file name is : {fileName}");
+        }
+        
+    }
+
+    public void PrintEveryPDFFileDetails(PDFFile[] files)
+    {
+        Console.WriteLine("File in files[]: ");
+        int i = 1;
+        foreach (PDFFile file in files)
+        {
+           // i += 1;
+            Console.WriteLine($"The file {i++}:");
+            Console.WriteLine($"file name is : {file.FileName}");
+            Console.WriteLine($"file name is : {file.FileSize}");
+            Console.WriteLine($"file name is : {file.FileLocation}");
+            Console.WriteLine($"file name is : {file.CreatedDate}");
+        }
+    }
+    public PDFFile getFirstPdf(PDFFile[] files)
+    {
+        return files[0];
+    }
+
+}
+
+
+class PDFProgram
+{
+
+    static void Main()
+    {
+        PDFFilesSystem pdf = new PDFFilesSystem();
+
+        string[] fileNames = { "PDFFile1.pdf", "PDFFile2.pdf", "PDFFile3.pdf" };
+
+        pdf.ShowPDFFileNamesLength(fileNames);
+
+        pdf.PrintEveryPDFFileName(fileNames);
+
+        PDFFile[] allCreditCardStatementFiles = new PDFFile[]
+        {
+            new PDFFile()
+                {
+                    FileName = "CreditcardstatmentMarch.pdf",
+                    CreatedDate = "Mar-01-2025",
+                    FileSize = "5kb",
+                    FileLocation = @"C:/Downloads"
+                },
+            new PDFFile()
+                {
+                    FileName = "CreditcardstatmentApril.pdf",
+                    CreatedDate = "April-01-2025",
+                    FileSize = "4kb",
+                    FileLocation = @"C:/Downloads"
+                },
+            new PDFFile()
+                {
+                    FileName = "CreditcardstatmentMay.pdf",
+                    CreatedDate = "May-01-2025",
+                    FileSize = "3kb",
+                    FileLocation = @"C:/Downloads"
+                }
+          };
+
+        pdf.PrintEveryPDFFileDetails(allCreditCardStatementFiles);
+
+        PDFFile p1 = pdf.getFirstPdf(allCreditCardStatementFiles);
+        Console.WriteLine($"The first file name is : {p1.FileName}");
+    }
+
+}
---
+++ Csharp-Step2/5-Constructors.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class FileDetailsConstructors
+{
+
+    string filename = "Student1_Resume.docx";
+    string filelocation = @"D:\Students\Resumes";
+    string filesize = "2Kb";
+    string createddate = "Mar-01-2024";
+    DateTime createdDateTime = DateTime.Now;
+
+    //0 param constructor
+    public FileDetailsConstructors()
+    {
+        Console.WriteLine("File object created");
+        Console.WriteLine(this.filename); // "Student1_Resume.docx"
+        Console.WriteLine(this.createdDateTime);
+        this.filename = "Sam_resume.docx"; //changing Student_Resume to Sam_resume
+
+    }
+    //2 param constructor
+    public FileDetailsConstructors(string filename,string filesize)
+    {
+        this.filename = filename;
+        this.filesize = filesize;
+        Console.WriteLine("Param Constructor called.\nFile object created");
+        Console.WriteLine(this.filename);
+        Console.WriteLine(this.filesize);
+        Console.WriteLine(this.createdDateTime);
+    }
+
+    static void Main()
+    {
+        FileDetailsConstructors file1 = new FileDetailsConstructors();
+
+        //Note :  changed the filename inside of the constrcutor.
+
+        Console.WriteLine(file1.filename);  //Sam_resume.docx
+
+        FileDetailsConstructors file2 = new FileDetailsConstructors("File 2 CV","44Kb");
+
+    }
+
+
+
+
+}
