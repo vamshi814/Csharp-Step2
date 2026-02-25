7afe5f22272ed1d879f53a837326fb02b652073a
vamshi814 <vamshi814.s@gmail.com>
2026-02-25 16:51:07 +0530

    Abstraction

---
+++ Csharp-Step2/Abstraction/1Abstraction-Demo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// in interface every member is abstract so no need
+//in abstract class , we must use abstract keyword for abstract methods(implemented later)
+
+namespace AbstractionSystem
+{
+
+
+    abstract class MSG{
+        //Abstract Properties
+        public abstract int age { get; set; }
+
+        //Abstract Methods
+        public abstract void Msg();
+
+        //Normal Methods
+        public void getMsg()
+        {
+            Console.WriteLine("GetMsg");
+        }
+
+        // 0 or Param Cconstructors
+        public MSG()
+        {
+
+        }
+        public MSG(int age)
+        {
+            this.age = age;
+        }
+
+    }
+
+
+
+
+
+}
---
+++ Csharp-Step2/Abstraction/2Vehicle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace AbstractionVehicle
+{
+    abstract class Vehicle
+    {
+        public Vehicle(int v, string fuel)
+        {
+            Capacity = v;
+            FuelType = fuel;
+        }
+        public Vehicle() { }
+
+        public string FuelType { get; set; }
+        public int Capacity { get; set; }
+        
+
+        public abstract void start();
+        public abstract void Engine();
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Fuel type   : {FuelType}");
+            Console.WriteLine($"Capacity is : {Capacity}");
+        }
+    }
+    class Car : Vehicle
+    {
+        
+
+        public Car(int v, string f) : base(v,f)
+        {
+            
+        }
+
+        public override void start()
+        {
+            Console.WriteLine("Car started.");
+        }
+        public override void Engine()
+        {
+            Console.WriteLine("4 Cylinder Engine");
+        }
+    }
+    class Bike : Vehicle
+    {
+        public override void Engine()
+        {
+            Console.WriteLine("2 stroke engine");
+        }
+
+        public override void start()
+        {
+            Console.WriteLine("Bike started.");
+        }
+    }
+    class AbstractProgram
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Car(8,"Diesel");
+            //v.FuelType = "Diesel";
+            //v.Capacity = 5;
+            v.start();
+            v.Engine();
+            v.ShowDetails();
+
+            Console.WriteLine("-----------------------------");
+            Vehicle dio = new Bike();
+            dio.FuelType = "Petrol";
+            dio.Capacity = 2;
+            dio.start();
+            dio.Engine();
+            dio.ShowDetails();
+
+        }
+    }
+
+}
