addd20fd514ce343485d218bd4b0d9d7426d2588
vamshi814 <vamshi814.s@gmail.com>
2026-02-26 10:16:23 +0530

    Polymorphism

---
+++ Csharp-Step2/Polymorphism/1CompileTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class CompileTime
+{
+
+    public double area(double x, double y)
+    {
+        return x * y;   
+    }
+    public  double area(double a)
+    {
+        return 3.14 * a * a;
+    }
+    static void Main()
+    {
+        CompileTime ct = new CompileTime();
+        double RectArea = ct.area(2, 4);
+        Console.WriteLine(RectArea);
+        double CircleArea = ct.area(5);
+        Console.WriteLine(CircleArea);
+    }
+}
---
+++ Csharp-Step2/Polymorphism/2Runtime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+namespace Runtime
+{
+    //abstract class cannot be instatiated
+    //Runtime Polymorphism or Dynamic Polymorphism or Late binding
+    abstract class Vehicle
+    {
+        public abstract void start();
+    }
+    class Bike : Vehicle
+    {
+        public Bike() { }
+        public override void start()
+        {
+            Console.WriteLine("Bike");
+        }
+    }
+    class Car : Vehicle
+    {
+        public Car() { }
+        public override void start()
+        {
+            Console.WriteLine("Car");
+        }
+    }
+
+    class RuntimeMain
+    {
+        static void Main(string[] args) {
+            Vehicle c1 = new Bike();
+            c1.start();
+            Vehicle c2 = new Car();
+            c2.start();
+        }
+    }
+}
